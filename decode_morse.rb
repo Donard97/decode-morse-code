f43ac3fad7db10ca4475882e27fdd5595a1705d8
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2021-11-11 16:06:13 +0430

    write decode_char and decode functions

--- decode_morse.rb
+++ decode_morse.rb
@@ -35,4 +35,22 @@ def decode_char(str)
   end
 end
 
+def decode_word(str)
+  arr = []
+  str.split.each do |n|
+    arr.push(MORSE_CODE[n])
+  end
+  arr.join
+end
+def decode(str)
+  word = ''
+  str.strip.tr('/', ' ').split('   ').each do |n|
+    word += "#{decode_word(n)} "
+  end
+  word
+end
+
 puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
